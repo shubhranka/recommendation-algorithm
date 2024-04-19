2052b1bcf36e77fcb856734ae5e68fb07029a7d7
Shubhranka <shubhrankavarma@gmail.com>
2024-04-20 05:01:17 +0530

    added mongodb database

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello")
+// Connect to the database
+import { connect } from 'mongoose';
+await connect(process.env.MONGODB_CONNECTION_STRING)
+
+console.log('Connected to the database');
+
